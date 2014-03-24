3b0673a28aee082782afde5598af0becf0c5177d
Vova Abdrakhmanov <369565@gmail.com>
2014-03-24 22:17:53 +0400

    pep8

--- solver.py
+++ solver.py
@@ -9,7 +9,7 @@ plt.rc("text.latex", preamble="\\usepackage[utf8x]{inputenc}\
                                \\usepackage[russian]{babel}\
                                \\usepackage{amsmath}\
                                \\usepackage{pscyr}")
-plt.rc("figure", figsize = (4, 4))
+plt.rc("figure", figsize=(4, 4))
 
 # внешние параметры и постоянные
 q = 1.6e-19
@@ -31,9 +31,9 @@ D = u * k * T / q / abs(z)
 
 # параметры разностной схемы
 nodes = 101
-r = .25 # <.5 для сходимости схемы
+r = .25  # <.5 для сходимости схемы
 xi = linspace(0, 1, nodes)
-dxi = 1 / ( nodes - 1 )
+dxi = 1 / (nodes - 1)
 tau = 0
 dtau = r * dxi * dxi
 
@@ -48,17 +48,20 @@ abscissa = 1e9 * x
 #  j  | …   i-1   i   i+1   …
 #
 # он рассчитывает значение
-def linear_field_operator( c, i ):
+def linear_field_operator(c, i):
     w = q * abs(z) * phi / k / T
     s = .5 * w * dxi
     return c[i] * (1 - 2 * r) + c[i + 1] * r * (1 - s) + c[i - 1] * r * (1 + s)
 
-def linear_conc_operator( c, i ):
+
+def linear_conc_operator(c, i):
     w = q * abs(z) * phi / k / T
     s = .5 * w * dxi
     b = w * dtau
     g = lambda i: 1 / (xi[i] - c_out / (c_out - c_in))
-    return (c[i] * (1 - 2 * r + b * g(i)**2) + c[i + 1] * r * (1 - s * g(i)) + c[i - 1] * r * (1 + s * g(i)))
+    return (c[i] * (1 - 2 * r + b * g(i)**2) +
+            c[i + 1] * r * (1 - s * g(i)) +
+            c[i - 1] * r * (1 + s * g(i)))
 
 
 def linear_field_stat_conc(x):
@@ -71,7 +74,7 @@ def linear_conc_stat_conc(x):
     return c_out - (c_out - c_in) * x / thickness
 
 
-def main( name ):
+def main(name):
     if name == "linear_conc":
         stat_conc = linear_conc_stat_conc
         operator = linear_conc_operator
@@ -81,42 +84,43 @@ def main( name ):
         operator = linear_field_operator
 
     # устанавливаем начальное условие
-    c = list(map( start, linspace( 0, thickness, nodes ) ))
+    c = list(map(start, linspace(0, thickness, nodes)))
     c[0] = c_out
-    c[nodes-1] = c_in
+    c[-1] = c_in
     tau = 0
-
 
     # теперь считаем:
     step = 5e-9 * D / thickness ** 2
     new_c = c
 
-    for j in range(4):
+    for j in range(1, 5):
         while tau < step * j:
-            for i in range(2, nodes-1):
-                new_c[i] = operator( c, i )
+            for i in range(1, nodes-1):
+                new_c[i] = operator(c, i)
             c = new_c
             tau += dtau
 
         plt.plot(abscissa, c, color="black", linewidth=.5, linestyle="-")
-        if j == 3:
-            plt.annotate("\\(t = %.0f~\\text{нс}\\)" % t(tau) * 1e9,
-                xy = (abscissa[50], c[50]), xytext = (abscissa[50], c[50] + 30),
-                arrowprops = {"arrowstyle": "->",
-                              "color": "black",
-                              "shrinkB": .01})
+        if j == 4:
+            plt.annotate("\\(t = %.0f~\\text{нс}\\)" % (t(tau) * 1e9),
+                         xy=(abscissa[50], c[50]),
+                         xytext=(abscissa[50], c[50] + 30),
+                         arrowprops = {"arrowstyle": "->",
+                                       "color": "black",
+                                       "shrinkB": .01})
 
-    s_conc = list(map( stat_conc, x ))
+    s_conc = list(map(stat_conc, x))
     plt.plot(abscissa, s_conc, color="red")
-    plt.annotate("\\(t \\to \\infty \\)", xy = (abscissa[70], s_conc[70]),
-                xytext = (abscissa[70], s_conc[70] + 30),
-                arrowprops = {"arrowstyle": "->",
-                              "color": "red",
-                              "shrinkB": .01})
+    plt.annotate("\\(t \\to \\infty \\)", xy=(abscissa[70], s_conc[70]),
+                 xytext=(abscissa[70], s_conc[70] + 30),
+                 arrowprops={"arrowstyle": "->",
+                             "color": "red",
+                             "shrinkB": .01})
 
     plt.xlabel("Мембрана, нм")
-    plt.ylabel("Концентрация иона \\( \\mathrm{Na}^+ \\), моль/\\(\\text{м}^3\\)")
-    plt.title("Распределение ионов \\( \\mathrm{Na}^+ \\) внутри мембраны")
+    plt.ylabel("Концентрация иона \\(\\mathrm{Na}^+ \\),\
+                моль/\\(\\text{м}^3\\)")
+    plt.title("Распределение ионов \\(\\mathrm{Na}^+ \\) внутри мембраны")
     plt.savefig("plots/" + name + ".pdf")
     plt.cla()
 
