df869e13a3602a3f399d84e2e250ef69bfcc34d1
Vova Abdrakhmanov <369565@gmail.com>
2014-04-06 09:57:47 +0400

    ???

--- plotter.py
+++ plotter.py
@@ -1,34 +1,32 @@
-PRODUCTION = True
-
 from data import *
 from matplotlib import pyplot as plt
 
-if PRODUCTION:
-    plt.rc("text", usetex=True)
-    plt.rc("font", family="serif", size=14)
-    plt.rc("text.latex", unicode=True)
-    plt.rc("text.latex", preamble="\\usepackage[utf8x]{inputenc}\
-                               \\usepackage[T2A]{fontenc}\
-                               \\usepackage[russian]{babel}\
-                               \\usepackage{amsmath}\
-                               \\usepackage{pscyr}")
 
+plt.rc("text", usetex=True)
+plt.rc("font", family="serif", size=14)
+plt.rc("text.latex", unicode=True)
+plt.rc("text.latex", preamble="\\usepackage[utf8x]{inputenc}\
+                           \\usepackage[T2A]{fontenc}\
+                           \\usepackage[russian]{babel}\
+                           \\usepackage{amsmath}\
+                           \\usepackage{pscyr}")
 
-for E, l in [(E0, "Na"), (E1, "K"), (E2, "Cl")]:
-    plt.plot(abscissa, E, label=l)
+labels = ["$\mathrm{Na}^+$", "$\mathrm{K}^+$", "$\mathrm{Cl}^-$"]
+for i, e in enumerate(E):
+    plt.plot(abscissa, e, label=labels[i])
 
 #E = [E0[i] + E1[i] + E2[i] for i in range(len(abscissa))]
 #plt.plot(abscissa, E, "k-", label="ions' field")
 
 plt.xlabel(r"$x,\ \text{нм}$")
-plt.ylabel(r"$E,\ 10^9\frac{\text{В}}{\text{м}}$")
+plt.ylabel(r"$E,\ \frac{\text{В}}{\text{м}}$")
 plt.grid(True)
 plt.legend()
 plt.savefig("plots/stat_field.pdf")
 plt.cla()
 
-for C, l in [(C0, "Na"), (C1, "K"), (C2, "Cl")]:
-    plt.plot(abscissa, C, label=l)
+for i, c in enumerate(C):
+    plt.plot(abscissa, c, label=labels[i])
 plt.xlabel(r"$x,\ \text{нм}$")
 plt.ylabel(r"$C,\ \frac{\text{моль}}{\text{м}^3}$")
 plt.grid(True)
--- solver.cpp
+++ solver.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstdlib>
 #include <vector>
 #include <cmath>
 
@@ -12,11 +13,12 @@ typedef vector<vd> vvd;
 
 //# внешние параметры и постоянные
 const double q = 1.6e-19;
-const double F = 96485 * 1e-9;
+const double F = 96485;
 const double k = 1.38e-23;
 const double T = 298;
 const double eps0 = 8.85e-12;
 
+const int types_used = 3;
 //# параметры мембраны
 //                 Na   K   Cl
 const vd c_out = {430,  20, 556};
@@ -30,16 +32,18 @@ const double eps = 1;
 //# параметры ионов
 const vd u = {4.44e-8, 6.53e-8, 6.59e-8};
 const vd z = {1, 1, -1};
-vd D(3);
+vd D(types_used);
 //# параметры разностной схемы
-size_t nodes = 101;
-double r = .1;  // # <.5 для сходимости схемы
+size_t nodes = 4001;
+double r = 1e-2;  // # <.5 для сходимости схемы
 double dxi = 1.0 / ( nodes - 1 );
 double tau = 0;
 double dtau = r * dxi * dxi;
 
-vvd E(3);
-vvd C(3);
+vvd E(types_used);
+vvd C(types_used);
+vd xi(nodes);
+
 
 double t()
 {
@@ -51,7 +55,7 @@ double field_operator(size_t type, size_t i)
     double a = D[type] / D[0] / dxi / dxi * dtau;
     double b = z[type] / abs(z[type]) * u[type] * thickness / 2 / D[0] / dxi * dtau;
     double sumE = 0;
-    forn(k, 3)
+    forn(k, types_used)
         sumE += E[k][i];
     return E[type][i] * (1 - 2 * a) +
            E[type][i + 1] * (a - b * sumE) + E[type][i - 1] * (a + b * sumE);
@@ -59,17 +63,22 @@ double field_operator(size_t type, size_t i)
 
 void border_condition()
 {
-    forn(type, 3)
+    forn(type, types_used)
     {
         E[type][0] = E[type][1] - dxi * thickness * z[type] * F / eps / eps0 * c_out[type];
         E[type][nodes-1] = E[type][nodes-2] + dxi * thickness * z[type] * F / eps / eps0 * c_in[type];
+        double correction = (E[type][0] + E[type][nodes-1]) / 2;
+        forn(i, nodes)
+            E[type][i] -= correction;
     }
 }
 
 
 void init()
 {
-	forn(type, 3)
+    forn(i, nodes)
+        xi[i] = (double) i / (nodes - 1);
+	forn(type, types_used)
     {
         D[type] = u[type] * k * T / q / abs(z[type]);
         E[type] = vd(nodes);
@@ -79,24 +88,25 @@ void init()
     tau = 0;
 }
 
-void calculate_field()
+void calculate_field(double time)
 {
     vvd new_E = E;
-    while (t() < 1e-10)
+    while (t() < time)
     {
-        forn(type, 3)
+        forn(type, types_used)
             formn (i, 1, nodes-1)
                 new_E[type][i] = field_operator(type, i);
         E = new_E;
         border_condition();
         tau += dtau;
-        //printf("%e s\n", t());
+        printf("%.1f%% complete\n", t() / time * 100);
+        printf("\033[F\033[J"); // update previous line
     }
 }
 
 void calculate_concentrations()
 {
-    forn(type, 3)
+    forn(type, types_used)
     {
         C[type][0] = c_out[type];
         C[type][nodes-1] = c_in[type];
@@ -107,46 +117,45 @@ void calculate_concentrations()
 
 int main(int argc, char const *argv[])
 {
-    vd xi(nodes);
-    for (size_t i = 0; i < nodes; i++)
-        xi[i] = (double) i / (nodes - 1);
-    vd x(nodes);
-    for (size_t i = 0; i < xi.size(); i++)
-        x[i] = thickness * xi[i];
-    vd abscissa(nodes);
-    for (size_t i = 0; i < xi.size(); i++)
-        abscissa[i] = 10 * xi[i];
-
     // устанавливаем начальное условие
     init();
 
     // теперь считаем:
-    calculate_field();
+    double t = 1e-9;
+    if (argc > 1)
+        t = atof(argv[1]);
+    calculate_field(t);
     calculate_concentrations();
 
+    printf("write results to data.py… ");
     FILE *data;
     data = fopen("data.py", "w");
     fprintf(data, "abscissa = [ ");
-    forn(i, abscissa.size())
-        fprintf(data, "%f, ", abscissa[i]);
+    forn(i, nodes)
+        fprintf(data, "%f, ", xi[i] * 10);
     fprintf(data, "]\n");
 
-    forn(type, 3)
+    fprintf(data, "E = [\n");
+    forn(type, types_used)
     {
-        fprintf(data, "E%d = [ ", type);
+        fprintf(data, "[");
         forn(i, nodes)
             fprintf(data, "%f, ", E[type][i]);
-        fprintf(data, "]\n");
+        fprintf(data, "],\n");
     }
+    fprintf(data, "]\n");
 
-    forn(type, 3)
+    fprintf(data, "C = [\n");
+    forn(type, types_used)
     {
-        fprintf(data, "C%d = [ ", type);
+        fprintf(data, "[");
         forn(i, nodes)
             fprintf(data, "%f, ", C[type][i]);
-        fprintf(data, "]\n");
+        fprintf(data, "],\n");
     }
-
+    fprintf(data, "]\n");
     fclose(data);
+    printf("done!\n");
+
     return 0;
 }
